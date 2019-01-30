5ed22853aa11d14295e9211f1cd9754d314e3426
e-mokeev <e.mokeev@dev.userstory.ru>
2019-01-30 15:26:20 +0700

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    moduleDirectories: ['node_modules', 'src'],
     moduleNameMapper: {
         '\\.(css|less)$': 'identity-obj-proxy',
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.js',
