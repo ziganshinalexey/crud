dabf607bf1f66b831b57a0ee6bc990cd6b293dba
e-mokeev <e.mokeev@dev.userstory.ru>
2019-04-19 17:13:05 +0700

    Удалил лишние настройки

--- develop/deployer/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /usr/bin/deployer ]; then
-  rm /tmp/install.sh
-  wget https://deployer.userstory.ru/install.sh -O /tmp/install.sh && bash /tmp/install.sh
-fi
-
-/usr/bin/deployer
